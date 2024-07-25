b318645e4beb253782d2e890e023bf8e96f68492
sanukumar <sanukumar1138@gmail.com>
2024-07-25 15:03:15 +0530

    updated redux link

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,9 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import "./i18n.js";
 
+// redux tutorial
+// https://www.youtube.com/watch?v=dOkkHHuFxjM
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
